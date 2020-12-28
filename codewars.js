c0c75a5234eb7915cb98a7f835cf6b01bbc813dd
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-12-27 22:20:04 -0800

    is my friend cheating question

--- codewars.js
+++ codewars.js
@@ -64,3 +64,29 @@ function toWeirdCase(string){
   }
   return wierdCase.join('')
   }
+
+  //IS MY FRIEND CHEATING QUESTION
+  // A friend of mine takes a sequence of numbers from 1 to n (where n > 0).
+  // Within that sequence, he chooses two numbers, a and b.
+  // He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+  // Given a number n, could you tell me the numbers he excluded from the sequence?
+  // The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+
+  function removeNb (n) {
+    let sequenceSum = 0
+    let table = {}
+    let complement
+    let answer = []
+    for(let i = 0; i <= n; i++){
+      sequenceSum += i
+      table[i] = true
+    }
+  
+    for(let i = 0; i <= n; i++){
+      complement = (sequenceSum - i)/(i+1)
+      if(table[complement] == true){
+        answer.push([i, complement])
+      }
+    }    
+  return answer
+    }
