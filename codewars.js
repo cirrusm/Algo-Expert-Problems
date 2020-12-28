2ac611442d362cda8a65006943b1ab0fd5d07750
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-12-27 20:45:13 -0800

    recipe question

--- codewars.js
+++ codewars.js
@@ -18,3 +18,19 @@ function toCamelCase(str){
   return convertedLetters.join('')
   
   }
+
+  //GIVEN TWO OBJECTS, AN INGREDIENT LIST AND A RECIPE LIST, SEE HOW MANY CAKES YOU CAN MAKE BASED ON THE INGREDIENTTS THAT YOU AHVE
+// EXAMPLE recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
+    // available = {sugar: 500, flour: 2000, milk: 2000};
+  function cakes(recipe, available) {
+    let leastIngredient = null
+  for(let ingredient in recipe){
+    let cakesWithIngredient = Math.floor(available[ingredient]/recipe[ingredient])
+    if(available[ingredient] == null || cakesWithIngredient == 0){
+      return 0
+    }else if(leastIngredient == null || leastIngredient > cakesWithIngredient){
+      leastIngredient = cakesWithIngredient
+    }
+    }
+  return leastIngredient
+    }
